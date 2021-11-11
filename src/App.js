135e8d046d444bbad1faae97742c20f726d1aad4
raghadquni <Raghad.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-11 16:58:43 +0300

    frontend3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import axios from "axios";
+import { Route } from "react-router-dom"
+import Nav from "./component/Nav";
+
 
 const App = () => {
 
+  return (
+    <>
+      <Nav />
+      </>
+  );
 }
 export default App;
