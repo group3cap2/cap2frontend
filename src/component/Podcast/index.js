a24112b6d3c457896a7f40ad13dab37c1ea6db2b
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-11-12 21:49:54 +0300

    podcast api

--- src/component/Podcast/index.js
+++ src/component/Podcast/index.js
@@ -1,23 +1,44 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router";
 import "./style.css";
 import Modal from "react-modal"
-
-
 const Podcast = () => {
     const [podcast, setPodcast] = useState([]);
+    const navigate = useNavigate();
   useEffect(() => {
     getPodcast();
   }, []);
-
   const getPodcast = async () => {
-    const response = await axios.get(
-      "http://itunes.apple.com/search?term=s&country=sa&media=podcast&limit=15"
-    );
-    console.log(response.data.results);
+      const response = await axios.get("http://localhost:5000/media/podcast");
+
+    // console.log(response.data.results);
     setPodcast(response.data.results);
-    console.log(response.data.results);
   };
+  return (
+    <div className="mediaWrapper">
+      <h1>Podcast</h1>
+      <div className="Podcast">
+        {podcast.map((item, i) => (
+          <div className="Pod" key={i} onClick={() => {
+            navigate("/Podcast/open", { state: item });
+          }}>
+            <img
+              key={`img-${i}`}
+              className="PodImg"
+              src={item.artworkUrl100}
+              alt={`Img-${i}`}
+            />
+            <p className="podName" key={`trackN-${i}`}>
+              <b>{item.trackName}</b>
+            </p>
+            <p className="artistName" key={`artN-${i}`}>
+              {item.artistName}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Podcast;
