a4a3980ce7aad30f6ffab18673875e8fe3a4598b
SaharAlassaf <Sahar.Hussain.elq.js@tuwaiq.edu.sa>
2021-11-13 18:36:39 +0300

    nav

--- src/component/Nav/index.js
+++ src/component/Nav/index.js
@@ -1,44 +1,36 @@
-import React , {useState} from "react";
-import { Link } from "react-router-dom";
+import React  from "react";
+import {NavLink, Link} from 'react-router-dom'
 import { useNavigate } from 'react-router';
 import "./style.css";
-
-
-
-const Nav = () => { 
-
-    
-    
-
+import { useLocation } from "react-router-dom";
+const Nav = () => {
+    const location = useLocation();
+    const { pathname } = location;
+    const splitLocation = pathname.split("/");
    /* const navigate = useNavigate()
     const goBack = () => {
         navigate.goBack()
-
     } */
-
-
-
-
     return (
         <div className="nav">
-
             <ul className="ulNav">
-                <li> <Link to ="/">All</Link></li>
-                <li> <Link to ="/Music">Music</Link></li>
-                <li> <Link to ="/Ebook">Ebook</Link></li>
-                <li> <Link to ="/Podcast">Podcast</Link></li>
-                <li> <Link to ="/Movie">Movies</Link></li>
-                <li> <Link to ="/Fav">You Love</Link></li>
-                { /*     <li> <Link to ="/Login">Login</Link></li> */}
-                 <li> <div className="search">
-           <input type="text" placeholder = "Search Podcast or Music "/>
-           { /* <button  placeholder="Search">Search</button> */}
-
-            </div></li> 
+                <li className={splitLocation[1] === "" ? "active" : ""}>
+                    <Link to ='/'>All</Link> </li>
+                <li className={splitLocation[1] === "Music" ? "active" : ""}>
+                    <Link to ='/Music'>Music</Link></li>
+                <li className={splitLocation[1] === "Ebook" ? "active" : ""}>
+                    <Link to ="/Ebook">Ebook</Link></li>
+                <li className={splitLocation[1] === "Podcast" ? "active" : ""}>
+                    <Link to ="/Podcast">Podcast</Link></li>
+                <li className={splitLocation[1] === "Movie" ? "active" : ""}>
+                    <Link to ="/Movie">Movies</Link></li>
+                <li className={splitLocation[1] === "Fav" ? "active" : ""}>
+                    <Link to ="/Fav">You Love</Link></li>
+                <li> <div className="search">
+                <input type="text" placeholder = "Search Podcast or Music "/>
+            </div></li>
             </ul>
         </div>
     )
-
 }
-
 export default Nav;
