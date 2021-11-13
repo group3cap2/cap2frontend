c02f1b7ef4bb94e8752c743e6a4427cd84212284
raghadquni <Raghad.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-13 16:16:47 +0300

    prprdddlلب

--- src/component/Pod/index.js
+++ src/component/Pod/index.js
@@ -1,80 +1,43 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import ReactPlayer from "react-player";
-import Modal from "react-modal";
+// import ReactPlayer from "react-player";
+// import Modal from "react-modal";
 import "./style.css";
 import { useParams } from "react-router-dom";
 // import { useNavigate } from "react-router-dom";
 
-function pod() {
+function Pod() {
   const id = useParams().trackId;
-  const [pod, setPod] = useState();
+  const [pod, setPod] = useState("");
   // let navigate = useNavigate();
 
   const getPodcast = async () => {
     const response = await axios.get("http://localhost:5000/media/podcast");
-    setPod(response.data.find((elem) => elem.trackId == id));
+  
+    setPod(response.data.results.find((elem) => elem.trackId == id));
   };
 
   useEffect(() => {
-    getPodcast;
+    getPodcast();
   }, []);
 
-  let subtitle;
-  const [modal, setModal] = React.useState(false);
-
-  function openModal() {
-    setModal(true);
-  }
-
-  useEffect(() => {
-    console.log("hi");
-  }, [modal]);
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#f00";
-  }
-
-  function closeModal() {
-    setModal(false);
-  }
-
+  
   return (
     <div className="Pod">
-        {pod.map((elem) => (
-          <div className="singleAudio">
-            <img
-              className="singleImg"
-              src={elem.artworkUrl100}
-              alt={`card ${elem.collectionName}`}
-            />
-        
-      <div className="headerModal">
-        <FaWindowClose id="closeIcon" onClick={closeModal} />
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
-          {elem.collectionName}
-        </h2>
-        <img id="gifM" src="https://i.pinimg.com/originals/5c/4a/1c/5c4a1cef8a1ebd3584fac99c817b173c.gif" />
+       {pod ? (
+        <div className="1Pod">
+          <div className="blackBackgroundM">h</div>
+          <p className="Podcast"> {pod.trackName}</p>
+          <img src={pod.artworkUrl100} alt="mmm" />
+          <audio controls className="audio">
+          <source src={pod.previewUrl} type="audio/mp4" />
+        </audio>
+          <h6> {pod.artistName}</h6>
         </div>
-
-        <img
-              className="modalImg"
-              src={elem.artworkUrl100}
-              alt={`card ${elem.collectionName}`}
-            />
-
-            <ReactPlayer
-                    url={elem.previewUrl}
-                    width="400px"
-                    height="50px"
-                    playing={false}
-                    controls={true}
-                    className="aduioController"
-            />
-</div>
+      ) : (
+        <h1>loading ...</h1>
+      )}
     
-        ))};
         </div>
   )}
 
